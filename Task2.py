04138e236093e36fcfb8f978efe371618ff280cc
SNvortex <niksvortex@gmail.com>
2019-10-02 01:24:19 +0300

    Added task#2

--- Task2.py
+++ Task2.py
@@ -1,7 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get('https://www.gethatch.com/en/')
+headers = requests.utils.default_headers()
+headers.update({
+    'User-Agent':
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'
+})
+
+url = 'https://www.gethatch.com/en/'
+response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
 
 for brand in soup.find_all('img', attrs={'class': r'qode_client_main_image'}):
